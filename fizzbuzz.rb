e6be26ba595dc129715dfac6576e6fa2295506ca
Widodo Putro <wd.putro@gmail.com>
2018-01-02 15:25:58 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,11 @@
-def fizzbuzz
-
+def fizzbuzz(inp)
+  if inp % 3 == 0
+    puts "Fizz"
+  elseif inp % 5 == 0
+    puts "Buzz"
+  elseif inp % 5 == 0 && inp % 3 == 0
+    puts "FizzBuzz"
+  else 
+    inp
+  end
 end
