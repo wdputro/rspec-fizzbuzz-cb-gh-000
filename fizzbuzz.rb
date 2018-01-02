8a0dc71985a01a95df28915359b4129f38f3c7e7
Widodo Putro <wd.putro@gmail.com>
2018-01-02 15:30:19 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(inp)
   if inp % 3 == 0
     return "Fizz"
-  elseif inp % 5 == 0
+  elsif inp % 5 == 0
     return "Buzz"
-  elseif inp % 5 == 0 and inp % 3 == 0
+  elsif inp % 5 == 0 and inp % 3 == 0
     return "FizzBuzz"
   else
     return inp
